b9a2c75f0255f6f9472277ba926ccbccc2469eab
Your Name <your-email@example.com>
2025-05-12 22:35:29 +0300

    aiclassify add translate commit

---
+++ translate.py
@@ -0,0 +1,12 @@
+translate = {
+    "cane": "Köpek",
+    "cavallo": "At",
+    "elefante": "Fil",
+    "farfalla": "Kelebek",
+    "gallina": "Tavuk",
+    "gatto": "Kedi",
+    "mucca": "İnek",
+    "pecora": "Koyun",
+    "scoiattolo": "Sincap",
+    "ragno": "Örümcek"
+}
